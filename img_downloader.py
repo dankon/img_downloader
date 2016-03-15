9aa2ae60740f50cc7e468280e610833bd1bdc444
dankon <daniel.konwisorz@gmail.com>
2016-03-15 14:27:21 +0100

    Refactoring of code

--- img_downloader.py
+++ img_downloader.py
@@ -7,10 +7,11 @@ The script requires requests module for working.
 import sys
 import requests
 
+
 def img_downlader(file_loc, img_destiantion='downloaded_imgs/'):
     """\
     To run downloader is necessary to provide file location (file_loc)
-    and optionally download image destination folder 
+    and optionally download image destination folder
     (img_destiantion, which default value is downloaded_imgs/).
     """
     #TODO: 20160315-01 fix problem with IOError
@@ -18,14 +19,13 @@ def img_downlader(file_loc, img_destiantion='downloaded_imgs/'):
         for url_line in urls_file.readlines():
             #TODO: 20160315-02 prepare checking if url_line is URL address
             url_line = url_line.strip()
-            print "downloading image from %s" % url_line
+            print "Downloading image from %s" % url_line
             img_location = img_destiantion + url_line.split('/')[-1]
             #TODO: 20160315-03 check response of requests
+            req_img = requests.get(url_line, verify=False, stream=True)
             with open(img_location, 'wb') as output_file:
                 #TODO: 20160315-04 fix issue with warnings
-                output_file.write(requests.get(url_line,
-                                               verify=False,
-                                               stream=True).content)
+                output_file.write(req_img.content)
                 print "Image has been downloaded to %s\n" % img_location
 
 
