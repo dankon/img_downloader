1bc98c09fc67b11e1f30352531bcc0b797864cda
dankon <daniel.konwisorz@gmail.com>
2016-03-15 12:58:10 +0100

    First working downloader with some TODOs

--- img_downloader.py
+++ img_downloader.py
@@ -2,25 +2,37 @@
 #-*- coding: utf-8 -*-
 """
 This is simple script for download all images by URLs in plaintext file.
+The script requires requests module for working.
 """
 import sys
+#TODO: 20160315-05 add dependencies
+import requests
 
-def img_downlader(file_loc, img_destiantion='/downloaded_imgs/'):
+def img_downlader(file_loc, img_destiantion='downloaded_imgs/'):
     """
     To run downloader is necessary to provide file location (file_loc)
     and optionally download image destination (img_destiantion, which
-    default value is /downloaded_imgs/).
+    default value is downloaded_imgs/).
     """
     #TODO: 20160315-01 fix problem with IOError
     with open(file_loc, 'r') as urls_file:
         for url_line in urls_file.readlines():
             #TODO: 20160315-02 prepare checking if url_line is URL address
             url_line = url_line.strip()
-            print "downloading image from %s \n" % url_line
+            print "downloading image from %s" % url_line
+            img_location = img_destiantion + url_line.split('/')[-1]
+            #TODO: 20160315-03 check response of requests
+            with open(img_location, 'wb') as output_file:
+                #TODO: 20160315-04 fix issue with warnings
+                output_file.write(requests.get(url_line,
+                                               verify=False,
+                                               stream=True).content)
+            print "image has been downloaded to %s\n" % img_location
+
 
 if __name__:
     print "Start program"
     f_location = sys.argv[1] if len(sys.argv) > 1 else raw_input("""\
-Please provide file name for URLs container:\n""")
+    Please provide file name for URLs container:\n""")
     img_downlader(f_location)
     print "Stop program"
