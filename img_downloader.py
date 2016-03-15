b4b03436454410f001d696fa4a3fb6b431396d00
dankon <daniel.konwisorz@gmail.com>
2016-03-15 11:52:04 +0100

    provide functionality for img_downloader function and add raw_input

--- img_downloader.py
+++ img_downloader.py
@@ -6,15 +6,19 @@ This is simple script for download all images by URLs in plaintext file.
 
 def img_downlader(file_loc, img_destiantion='/downloaded_imgs/'):
     """
-    To run downloader is necessary to provide text file location (file_loc)
-    and optionally download image destination (img_destiantion, which 
+    To run downloader is necessary to provide file location (file_loc)
+    and optionally download image destination (img_destiantion, which
     default value is /downloaded_imgs/).
     """
-    pass
-    
+    #TODO: 20160315-01 fix problem with IOError
+    with open(file_loc, 'r') as urls_file:
+        for url_line in urls_file.readlines():
+			#TODO: 20160315-02 prepare checking if url_line is URL address
+            print url_line
+
 
 if __name__:
     print "Start program"
-    file_loc = raw_input("Please provide file name for URLs container:\n")
-    img_downlader(file_loc)
+    f_location = raw_input("Please provide file name for URLs container:\n")
+    img_downlader(f_location)
     print "Stop program"
